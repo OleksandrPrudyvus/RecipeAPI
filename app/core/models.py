f49ac1f184f7052f5f3f0cea06898c6de4ccdc61
OleksandrPrudyvus <pryduvyso20@gmail.com>
2022-10-12 18:55:45 +0300

    Added new migrations file and test_models.py for test model. Created User model and implemented test for normalize email, check blank email and create new user instance.

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,33 @@
-from django.db import models  # noqa
+"""
+Database models.
+"""
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for user."""
+
+    def create_user(self, email, password=None, **extra_field):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email!!!')
+        user = self.model(email=self.normalize_email(email), **extra_field)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    objects = UserManager()
